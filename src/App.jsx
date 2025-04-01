547f2883e8d4ecfd55fe4d8349b2240f5d43488f
Tomás <tuemail@example.com>
2025-04-01 17:12:46 -0300

    Componente navigation menu agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Button } from "./components/ui/button"
+import { NavigationMenu } from "./components/ui/navigation-menu.jsx"
 import { Card } from "./components/ui/card"
+
 
 function App() {
   return (
     <>
       <main className="flex flex-col gap-4 items-center justify-center min-h-screen p-4 bg-background">
+      <NavigationMenu />
         <h1 className="font-bold text-3xl">Restaurante App</h1>
       </main>
     </>
