18119f5dc2042229dd17aace4fec79ab968d4399
Tomás <tuemail@example.com>
2025-04-01 17:56:36 -0300

    Componente header agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Button } from "./components/ui/button"
-import { NavigationMenu } from "./components/ui/navigation-menu.jsx"
+import  Header  from "./components/ui/Header.jsx"
 import { Card } from "./components/ui/card"
 
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <main className="flex flex-col gap-4 items-center justify-center min-h-screen p-4 bg-background">
-      <NavigationMenu />
+      <Header />
         <h1 className="font-bold text-3xl">Restaurante App</h1>
       </main>
     </>
