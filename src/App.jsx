c1e9df3427de482ebcee3f03221141b22a6fadc1
Tadeo <tadeomaddonni@gmail.com>
2025-04-01 11:14:09 -0300

    Initializing shadcn ui
    Co-Authored-By: rufinoratti <128641028+rufinoratti@users.noreply.github.com>
    Co-Authored-By: tomaslonati <104846309+tomaslonati@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from "./components/ui/button"
+import { Card } from "./components/ui/card"
 
 function App() {
   return (
     <>
-      <main>
-        <h1>Restaurante app</h1>
+      <main className="flex flex-col gap-4 items-center justify-center min-h-screen p-4 bg-background">
+        <h1 className="font-bold text-3xl">Restaurante App</h1>
       </main>
     </>
   )
