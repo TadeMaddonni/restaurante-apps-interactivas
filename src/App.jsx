c7acd95b8cf04d7c0f0922da5f54bcaea0e81c1a
rufinoratti <rufinoratti@gmail.com>
2025-04-06 20:23:50 -0300

    creating-our-enviroment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Header from "./components/common/Header.jsx";
 import Hero from "./components/sections/home/Hero";
 import SelectedDishes from "./components/sections/home/Selected-dishes";
+import OurEnvironment  from "./components/sections/home/our-enviroment.jsx";
+
 
 function App() {
 	return (
@@ -8,6 +10,7 @@ function App() {
 			<Header />
 			<main className="flex flex-col gap-4  justify-start min-h-screen  bg-background">
 				<Hero />
+				<OurEnvironment/>	
 				<SelectedDishes />
 			</main>
 		</>
