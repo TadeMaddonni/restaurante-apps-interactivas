0cd363dc9ccc11ebcc889abb687e0dcf3a3a257a
Tadeo Maddonni <tadeomaddonni@MacBook-Pro-de-Tadeo.local>
2025-05-01 20:02:45 -0300

    fixing routing caseSensitve issues

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Hero from "../sections/home/Hero";
 import SelectedDishes from "../sections/home/Selected-dishes";
 import OurEnvironment from "../sections/home/our-enviroment.jsx";
-import Location from "../sections/home/Location.jsx";
+import Location from "../sections/home/location.jsx";
 import Destacados from "../sections/home/Destacados.jsx";
 
 function Home() {
     return (
-        <div>
+        <div className="w-full">
             <Hero />
             <Destacados />
             <OurEnvironment />
